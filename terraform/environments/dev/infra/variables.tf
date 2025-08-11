37f18fa142ad807927069b7fd3f016230c5f8ed1
Daniel Semedo <34478209+dansemedo@users.noreply.github.com>
2025-08-11 18:00:45 -0300

    Update variables.tf

--- terraform/environments/dev/infra/variables.tf
+++ terraform/environments/dev/infra/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 variable "instance_type" {
   description = "EC2 instance type for development"
   type        = string
-  default     = "t2.nano"
+  default     = "t2.small"
 }
 
 variable "key_name" {
